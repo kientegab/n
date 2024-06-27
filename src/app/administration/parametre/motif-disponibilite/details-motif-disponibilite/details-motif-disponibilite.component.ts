3e9ede0681114d3dbb3cdec2915fca7e003b00b8
michaelyameogo <michaelyameogo31@gmail.com>
2024-06-27 04:02:50 -0800

    up

--- src/app/administration/parametre/motif-disponibilite/details-motif-disponibilite/details-motif-disponibilite.component.ts
+++ src/app/administration/parametre/motif-disponibilite/details-motif-disponibilite/details-motif-disponibilite.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { cloneDeep } from 'lodash';
+import { DynamicDialogConfig, DynamicDialogRef } from 'primeng/dynamicdialog';
+import { IMotifDisponibilite, MotifDisponibilite } from 'src/app/shared/model/motifDisponibilite.model';
 
 @Component({
   selector: 'app-details-motif-disponibilite',
@@ -6,5 +9,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./details-motif-disponibilite.component.scss']
 })
 export class DetailsMotifDisponibiliteComponent {
+
+  motifDisponibilite: IMotifDisponibilite = new MotifDisponibilite();
+  @Input() data: IMotifDisponibilite = new MotifDisponibilite();
+
+  constructor(
+    private dialogRef: DynamicDialogRef,
+    private dynamicDialog:  DynamicDialogConfig,
+) {}
+
+  ngOnInit(): void {
+    if (this.dynamicDialog.data) {
+      this.motifDisponibilite = cloneDeep(this.dynamicDialog.data);
+    }
+    }
+
+    clear(): void {
+      this.dialogRef.close();
+      this.dialogRef.destroy();
+  }
 
 }
