511b8ac5a927bdecda0028f8d3d434e740af8642
faycallenglengue200 <faycallenglengue200@gmail.com>
2024-06-27 13:23:38 +0200

    modelpieceDisponibilite

---
+++ src/app/administration/parametre/piece-disponibilite/piece-disponibilite.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-piece-disponibilite',
+  templateUrl: './piece-disponibilite.component.html',
+  styleUrls: ['./piece-disponibilite.component.scss']
+})
+export class PieceDisponibiliteComponent {
+
+}
