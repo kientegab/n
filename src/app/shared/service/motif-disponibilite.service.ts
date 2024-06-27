afd5459e019d11549f6f9e57ca9a562eb3d12a63
michaelyameogo <michaelyameogo31@gmail.com>
2024-06-27 01:25:56 -0800

    updateMotifDis

--- src/app/shared/service/motif-disponibilite.service.ts
+++ src/app/shared/service/motif-disponibilite.service.ts
@@ -2,12 +2,12 @@ import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { LazyLoadEvent } from 'primeng/api';
 import { Observable } from 'rxjs';
-import { IMotif } from '../model/motif.model';
 import { createRequestOption } from '../util/request-util';
 import { environment } from 'src/environments/environment';
+import { IMotifDisponibilite } from '../model/motifDisponibilite.model';
 
-type EntityResponseType = HttpResponse<IMotif>;
-type EntityArrayResponseType = HttpResponse<IMotif[]>;
+type EntityResponseType = HttpResponse<IMotifDisponibilite>;
+type EntityArrayResponseType = HttpResponse<IMotifDisponibilite[]>;
 
 
 //const motifUrl = "assets/data/motif.json";
@@ -20,26 +20,26 @@ export class MotifDisponibiliteService {
 
   constructor(private http:HttpClient) { }
 
-  create(motif: IMotif): Observable<EntityResponseType> {
-    return this.http.post<IMotif>(motifUrl+'/new', motif, { observe: 'response' });
+  create(motif: IMotifDisponibilite): Observable<EntityResponseType> {
+    return this.http.post<IMotifDisponibilite>(motifUrl+'/new', motif, { observe: 'response' });
   }
 
-  update(groupe: IMotif): Observable<EntityResponseType> {
-    return this.http.put<IMotif>(motifUrl+'/update', groupe, { observe: 'response' });
+  update(groupe: IMotifDisponibilite): Observable<EntityResponseType> {
+    return this.http.put<IMotifDisponibilite>(motifUrl+'/update', groupe, { observe: 'response' });
   }
 
   find(id: number): Observable<EntityResponseType> {
-    return this.http.get<IMotif>(`${motifUrl}/${id}`, { observe: 'response' });
+    return this.http.get<IMotifDisponibilite>(`${motifUrl}/${id}`, { observe: 'response' });
   }
 
   query(req?: any): Observable<EntityArrayResponseType> {
     const options = createRequestOption(req);
-    // return this.http.get<IMotif[]>(motifUrl+'/list-page', { params: options, observe: 'response' });
-    return this.http.get<IMotif[]>(motifUrl+'/list-page', { params: options, observe: 'response' });
+    // return this.http.get<IMotifDisponibilite[]>(motifUrl+'/list-page', { params: options, observe: 'response' });
+    return this.http.get<IMotifDisponibilite[]>(motifUrl+'/list-page', { params: options, observe: 'response' });
   }
 
    findAll(event?: LazyLoadEvent): Observable<EntityArrayResponseType> {
-    return this.http.get<IMotif[]>(motifUrl+'/list', { observe: 'response' });
+    return this.http.get<IMotifDisponibilite[]>(motifUrl+'/list', { observe: 'response' });
   }
 
   delete(id: number): Observable<HttpResponse<{}>> {
@@ -47,7 +47,7 @@ export class MotifDisponibiliteService {
   }
 
   findListe(): Observable<EntityArrayResponseType> {
-    return this.http.get<IMotif[]>(motifUrl+'/list', { observe: 'response' });
+    return this.http.get<IMotifDisponibilite[]>(motifUrl+'/list', { observe: 'response' });
   }
 
 }
