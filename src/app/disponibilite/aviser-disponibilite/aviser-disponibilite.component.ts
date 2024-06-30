193f01415130530050b8f3167eb19760d4e31b16
faycallenglengue200 <faycallenglengue200@gmail.com>
2024-07-01 00:05:15 +0200

    receptionDisponibilite

--- src/app/disponibilite/aviser-disponibilite/aviser-disponibilite.component.ts
+++ src/app/disponibilite/aviser-disponibilite/aviser-disponibilite.component.ts
@@ -1,10 +1,12 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Component, Input } from '@angular/core';
 import { cloneDeep } from 'lodash';
-import { ConfirmationService, SelectItem } from 'primeng/api';
+import { ConfirmationService, Message, SelectItem } from 'primeng/api';
 import { DynamicDialogRef, DynamicDialogConfig, DialogService } from 'primeng/dynamicdialog';
 import { IDemande, Demande } from 'src/app/shared/model/demande.model';
 import { Historique, IHistorique, AVIS } from 'src/app/shared/model/historique.model';
+import { DemandeDisponibiliteService } from 'src/app/shared/service/demande-disponibilite-service.service';
+import { TokenService } from 'src/app/shared/service/token.service';
 
 @Component({
   selector: 'app-aviser-disponibilite',
@@ -13,48 +15,44 @@ import { Historique, IHistorique, AVIS } from 'src/app/shared/model/historique.m
 })
 export class AviserDisponibiliteComponent {
 
-
   demande: IDemande = new Demande();
   @Input() data: IDemande = new Demande();
-  demandes: any;
   isDialogOpInProgress: boolean | undefined;
-  demandeService: any;
   isOpInProgress: boolean | undefined;
-  aviser!: boolean;
-  receptionner!: boolean;
+  isLoggedIn = false;
+  profil!: string;
   dialogErrorMessage: any;
-  typeDemandeService: any;
-  typeDemandes: any;
-  message: { severity: string; summary: any; } | undefined;
+  message: any;
+  timeoutHandle: any;
   historique:IHistorique = new Historique();
   avis = AVIS;
   
  
-
   constructor(
     private dialogRef: DynamicDialogRef,
     private dynamicDialog:  DynamicDialogConfig,
-    private dialogService: DialogService,
-    private confirmationService: ConfirmationService
+    private demandeService: DemandeDisponibiliteService,
+    private tokenService: TokenService
   ) {}
 
   ngOnInit(): void {
+
+    this.isLoggedIn = !!this.tokenService.getToken();
+    if (this.isLoggedIn) {
+      const user = this.tokenService.getUser();
+      this.profil = user.profil;
+    }
+
     if (this.dynamicDialog.data) {
       this.demande = cloneDeep(this.dynamicDialog.data);
     }
   }
     
-
   clear(): void {
-      this.dialogRef.close();
-      this.dialogRef.destroy();
+    this.dialogRef.close();
+    this.dialogRef.destroy();
   }
-  // avis: SelectItem[] = [
-  //   { label: 'Avis favorable ', value: avis.avis1 },
-  //   { label: 'Avis defavorable', value: avis.avis2 },
-  // ];
  
-
   // Errors
   handleError(error: HttpErrorResponse) {
     console.error(`Processing Error: ${JSON.stringify(error)}`);
@@ -62,61 +60,98 @@ export class AviserDisponibiliteComponent {
     this.dialogErrorMessage = error.error.title;
   }
   
-  saveEntity(): void {
+  aviserDemande(): void {
     this.clearDialogMessages();
     this.isDialogOpInProgress = true;
     if (this.demande) {
       if (this.demande.id) {
+        console.log("historique ===========", this.historique);
         this.demande.historique=this.historique;
-        this.demandeService.update(this.demande).subscribe(
-          {
-            next: (response: any) => {
-              this.dialogRef.close(response);
-              this.dialogRef.destroy();
-              this.showMessage({ severity: 'success', summary: 'demande modifié avec succès' });
-             
-            },
-            error: (error: { error: { message: any; }; }) => {
-              console.error("error" + JSON.stringify(error));
-              this.isOpInProgress = false;
-              this.showMessage({ severity: 'error', summary: error.error.message });
 
+        if(this.profil === "SH") {
+          this.demandeService.aviserSH(this.demande).subscribe(
+            {
+              next: (response: any) => {
+                this.dialogRef.close(response);
+                this.dialogRef.destroy();
+                this.showMessage({ severity: 'success', summary: 'Demande avisée avec succès' });
+              
+              },
+              error: (error: { error: { message: any; }; }) => {
+                console.error("error" + JSON.stringify(error));
+                this.isOpInProgress = false;
+                this.showMessage({ severity: 'error', summary: error.error.message });
+              }
             }
-          });
-      } else {
-        this.demandeService.create(this.demande).subscribe({
-          next: (response: any) => {
-            this.dialogRef.close(response);
-            this.dialogRef.destroy();
-            this.showMessage({
-              severity: 'success',
-              summary: 'demande creer avec succès',
-            });
-          },
-          error: (error: { error: { message: any; }; }) => {
-            console.error("error" + JSON.stringify(error));
-            this.isOpInProgress = false;
-            this.showMessage({ severity: 'error', summary: error.error.message });
+          );
+        }
 
-          }
-        });
+        if(this.profil === "DRH") {
+          this.demandeService.aviserDRH(this.demande).subscribe(
+            {
+              next: (response: any) => {
+                this.dialogRef.close(response);
+                this.dialogRef.destroy();
+                this.showMessage({ severity: 'success', summary: 'Demande avisée avec succès' });
+              
+              },
+              error: (error: { error: { message: any; }; }) => {
+                console.error("error" + JSON.stringify(error));
+                this.isOpInProgress = false;
+                this.showMessage({ severity: 'error', summary: error.error.message });
+              }
+            }
+          );
+        }
+
+        if(this.profil === "DGFP") {
+          this.demandeService.aviserDGFP(this.demande).subscribe(
+            {
+              next: (response: any) => {
+                this.dialogRef.close(response);
+                this.dialogRef.destroy();
+                this.showMessage({ severity: 'success', summary: 'Demande avisée avec succès' });
+              
+              },
+              error: (error: { error: { message: any; }; }) => {
+                console.error("error" + JSON.stringify(error));
+                this.isOpInProgress = false;
+                this.showMessage({ severity: 'error', summary: error.error.message });
+              }
+            }
+          );
+        }
+
+        if(this.profil === "SG") {
+          this.demandeService.aviserSG(this.demande).subscribe(
+            {
+              next: (response: any) => {
+                this.dialogRef.close(response);
+                this.dialogRef.destroy();
+                this.showMessage({ severity: 'success', summary: 'Demande avisée avec succès' });
+              
+              },
+              error: (error: { error: { message: any; }; }) => {
+                console.error("error" + JSON.stringify(error));
+                this.isOpInProgress = false;
+                this.showMessage({ severity: 'error', summary: error.error.message });
+              }
+            }
+          );
+        }
       }
     }
-  }clearDialogMessages() {
-    throw new Error('Method not implemented.');
-  }
-showMessage(arg0: { severity: string; summary: string; }) {
-    throw new Error('Method not implemented.');
   }
   
-  loadTypeDemande() {
-    this.typeDemandeService.findAll().subscribe((response: { body: any; }) => {
+  clearDialogMessages() {
+    this.dialogErrorMessage = null;
+  }
 
-      this.typeDemandes = response.body!;
-    }, (error: { error: any; }) => {
-      this.message = { severity: 'error', summary: error.error };
-      console.error(JSON.stringify(error));
-    });
+  showMessage(message: Message) {
+    this.message = message;
+    this.timeoutHandle = setTimeout(() => {
+      this.message = null;
+    }, 5000);
   }
-  
+
 }
