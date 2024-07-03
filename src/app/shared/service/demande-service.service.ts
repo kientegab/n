527e953d0ce012eb75c314a5eada1a4d59e28ebd
faycallenglengue200 <faycallenglengue200@gmail.com>
2024-07-03 16:58:19 +0200

    BTNDownload

--- src/app/shared/service/demande-service.service.ts
+++ src/app/shared/service/demande-service.service.ts
@@ -160,12 +160,17 @@ export class DemandeService {
         return this.http.post<IDemande>(`${demandeUrl}/signer-projet/${demande.id}`, demande.historique, { observe: 'response' });
     }
 
-generateRecipisse(demandeId: number): Observable<Blob> {
-  return this.http.get(`${exportUrl}/recepisse-demande/${demandeId}`, { responseType: 'blob' });
-}
+  generateRecipisse(demandeId: number): Observable<Blob> {
+    return this.http.get(`${exportUrl}/recepisse-demande/${demandeId}`, { responseType: 'blob' });
+  }
+  downloadActe(id: number): Observable<Blob> {
+    return this.http.get(`${demandeUrl}/download/${id}`, { responseType: 'blob' });
+  }
+  
+  
 
 
-    imputerCST(id: number, matriculeImputation: string, groupe: IDemande): Observable<EntityResponseType> {
+ imputerCST(id: number, matriculeImputation: string, groupe: IDemande): Observable<EntityResponseType> {
       return this.http.post<IDemande>(
         `${demandeUrl}/imputer/${id}/${matriculeImputation}`,
         groupe.historique,
@@ -176,7 +181,6 @@ generateRecipisse(demandeId: number): Observable<Blob> {
 
 
     }
-
 
 
 }
